e0f58c04bff1e8ef445a5275ad18cb78c98aa7be
z1-ly <3394581969@qq.com>
2026-08-31 09:29:45 +0800

    Delete src/main/java/com/ecovoice/repository directory

--- src/main/java/com/ecovoice/repository/NaturalEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecovoice.repository;
-
-import com.ecovoice.domain.NaturalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NaturalEntityRepository extends JpaRepository<NaturalEntity, Long> {
-}
